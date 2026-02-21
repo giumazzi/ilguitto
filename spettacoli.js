ea46fe36db3b4fe7985e9d793f923a6817bcc98f
giumazzi <giumazzi@gmail.com>
2026-02-21 20:48:37 +0100

    aggiornamento con creazione della pagina relativo al nuovo archivio spettacoli. nuovo file spettacoli.js e spettacoli.html

---
+++ spettacoli.js
@@ -0,0 +1,23 @@
+const dataSpettacoli = [
+    {
+        titolo: "Era mej quand giva peg",
+        anno: "1984",
+        regia: "Franco Scatasta",
+        descrizione: "Il primo grande successo ufficiale della compagnia, vincitore della rassegna 'A teatro con la Fortuna'.",
+        immagine: "immagini/guittologo.jpg"
+    },
+    {
+        titolo: "Un piat de sardel",
+        anno: "1994",
+        regia: "Ortensio Rivelli",
+        descrizione: "Una svolta importante nella produzione della compagnia, uno spaccato di vita e tradizione.",
+        immagine: "immagini/guittologo.jpg"
+    },
+    {
+        titolo: "Rumori fuori scena",
+        anno: "1996",
+        regia: "Augusto Spadoni",
+        descrizione: "Un adattamento di grande successo critico che ha elevato la qualità delle produzioni.",
+        immagine: "immagini/guittologo.jpg"
+    }
+];
